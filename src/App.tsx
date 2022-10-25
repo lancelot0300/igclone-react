7c4b0bc5d6cefd009bb3de8405acb3e5a30b1181
masza221 <80392735+masza221@users.noreply.github.com>
2022-10-25 17:24:37 +0200

    Login and Register done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import { Route, Routes } from "react-router-dom";
+import styled, { createGlobalStyle } from "styled-components";
+import Menu from "./components/Menu/Menu";
+import { Login } from "./components/Pages/Login/Login";
+import { Register } from "./components/Pages/Register/Register";
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+  *{
+    box-sizing:border-box;
+  }
+  body {
+    background-color: rgb(15 23 42);
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+`;
+
+const AppContainer = styled.div`
+  min-width: 250px;
+`;
+
+
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <GlobalStyle />
+      <Menu />
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
+    </AppContainer>
   );
-}
+};
 
 export default App;
---
+++ src/components/Pages/Login/Login.styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const LoginContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  height: 90vh;
+  min-height: 320px;
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  font-size: 1.3rem;
+  padding: 30px;
+`;
+
+export const FormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`;
+
