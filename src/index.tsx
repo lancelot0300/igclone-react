0e44783d64f63d97b334fc79ffd1d6be8043e5ca
masza221 <80392735+masza221@users.noreply.github.com>
2022-11-08 20:26:29 +0100

    add login

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./state/store";
+import  store  from "./state/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
