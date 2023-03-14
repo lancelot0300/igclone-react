aa8729a560236c2ec69cd4020a6346d3a0201f14
masza221 <masza221@gmail.com>
2023-03-14 16:03:04 +0100

    image types add

--- src/components/ChangeAvatar/ChangeAvatar.tsx
+++ src/components/ChangeAvatar/ChangeAvatar.tsx
@@ -1,39 +1,38 @@
-import { ChangeEvent, FC, useState } from 'react'
-import useFirebase from '../../hooks/useFirebase';
-import { IUser } from '../../interfaces/interfaces';
-import { Input } from '../Input/Input'
-import { InputWrapper, StyledChangeEmail} from './ChangeAvatar.styles'
+import { ChangeEvent, FC, useState } from "react";
+import { Form } from "../Form/Form";
+import useFirebase from "../../hooks/useFirebase";
+import { IUser } from "../../interfaces/interfaces";
+import { Input } from "../Input/Input";
+import { InputWrapper, StyledChangeEmail } from "./ChangeAvatar.styles";
 
 interface IProps {
   user: IUser;
 }
 
-const ChangeEmail:FC<IProps> = ( {user}) => {
-
+const ChangeEmail: FC<IProps> = ({ user }) => {
   const { updateAvatar } = useFirebase();
   const [loading, setLoading] = useState(false);
-    const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
-        setLoading(true);
-        if (!e.target.files) return;
-        await updateAvatar(e.target.files[0], user.uid);
-        setLoading(false);
-      };
+  const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    setLoading(true);
+   
+    setLoading(false);
+  };
 
   return (
     <StyledChangeEmail>
-        <p>Change Avatar:</p>
-        <InputWrapper>
+      <p>Change Avatar:</p>
+        <InputWrapper> 
           <span>Click or Drop File</span>
           <Input
-                  onChange={handleChange}
-                  type="file"
-                  name="avatar"
-                  accept="image/*"
-                  disabled={loading}
-         />
+            onChange={handleChange}
+            type="file"
+            name="avatar"
+            accept="image/*"
+            disabled={loading}
+          />
         </InputWrapper>
-      </StyledChangeEmail>
-  )
-}
+    </StyledChangeEmail>
+  );
+};
 
-export default ChangeEmail
+export default ChangeEmail;
